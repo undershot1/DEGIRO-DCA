745de2b9de2229008daee9a1b5f9c7ff069fc153
jdhenshaw <jonathan.d.henshaw@gmail.com>
2022-06-24 10:54:52 +0200

    minor tweaks added comments

--- DEGIRO-DCA.py
+++ DEGIRO-DCA.py
@@ -5,30 +5,41 @@ import degiroapi
 from degiroapi.product import Product
 from degiroapi.order import Order
 
+# set key parameters
 DEGIRO_USER = os.environ['DEGIRO_USER']
 DEGIRO_PASS = os.environ['DEGIRO_PASS']
 PRODUCT = os.environ['PRODUCT']
 REGULAR_AMOUNT = float(os.environ['REGULAR_AMOUNT'])
 
 if __name__ == "__main__":
+    # login
     degiro = degiroapi.DeGiro()
     degiro.login(DEGIRO_USER, DEGIRO_PASS)
 
     cashfunds = degiro.getdata(degiroapi.Data.Type.CASHFUNDS)
+    # get current available balance
     balance = float([s for s in cashfunds[0].split()][1])
+    # determine the investment amount which will be the minimum of the current
+    # balance and the regular amount to invest
     investment_amount = min(balance, REGULAR_AMOUNT)
 
+    # retrieve product information
     products = degiro.search_products(PRODUCT)
     product_id = Product(products[0]).id
     product_price = degiro.real_time_price(product_id, degiroapi.Interval.Type.One_Day)[0]['data']['lastPrice']
 
+    # run purchase
     if investment_amount >= product_price:
+        # determine quantity
         qty = math.floor(investment_amount / product_price)
+        # create the buy order
         degiro.buyorder(Order.Type.MARKET, product_id, 1, qty)
+        # output
         print('%s - ORDER SUBMITTED FOR %i @ EUR %.2f = EUR %.2f' % (
         datetime.date.today(), product_price, qty, qty * product_price))
     else:
+        # you ain't got enough brass
         print('%s - NOT ENOUGH FUNDS AVAILABLE (EUR %.2f), MIN. REQUIRED = EUR %.2f' % (
-        datetime.date.today(), investment_amount, product_price))
+        datetime.date.today(), 0.0, product_price))
 
     degiro.logout()
